008e1b701a2bc81a781f9dba76919f3a9e358d29
BuntinMA_Home <BuntinMA@mail.ru>
2022-11-13 22:11:24 +0300

    Запустил бота. Работает.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import telebot
+from telebot import TeleBot
+
 # t.me/BuntinMA_calculator_Bot.
-
 bot = telebot.TeleBot('5533837130:AAGroDJvDdmvAH1Dzrf1xNZzFKplyL7FA9A')
+@bot.message_handler()
+def echo(msg: telebot.types.Message):
+    bot.send_message(chat_id=msg.from_user.id, text=f"Вы прислали: {msg.text}")
 
+
+bot.polling()
