9dbf8680be0fce5b8ed541a083756d6f75acd83e
BuntinMA_Home <BuntinMA@mail.ru>
2022-11-13 22:26:21 +0300

    Первая рабочая функция.

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from telebot import TeleBot
 
 # t.me/BuntinMA_calculator_Bot.
 bot = telebot.TeleBot('5533837130:AAGroDJvDdmvAH1Dzrf1xNZzFKplyL7FA9A')
+
+
+def summator(text):
+    lst = text.split()
+    if len(lst) == 2 and lst[0].isdigit and lst[1].isdigit:
+        return str(int(lst[0]) + int(lst[1]))
+    return "error"
+
+
 @bot.message_handler()
 def echo(msg: telebot.types.Message):
-    bot.send_message(chat_id=msg.from_user.id, text=f"Вы прислали: {msg.text}")
+    bot.send_message(chat_id=msg.from_user.id, text=summator(msg.text))
 
 
 bot.polling()
